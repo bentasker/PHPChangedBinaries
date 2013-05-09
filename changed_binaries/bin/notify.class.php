165f7d74f4c968ab01df9432b4db2dc927978a1b
Ben Tasker <github@bentasker.co.uk>
2013-05-09 13:27:37 +0100

    Added de-structor

--- changed_binaries/bin/notify.class.php
+++ changed_binaries/bin/notify.class.php
@@ -3,28 +3,35 @@
 
 
 class changedbinariesNotify{
-
+   protected $alarms = array();
+   protected $warnings = array();
+   protected $infos = array();
+    
 
   function alarm($str){
     echo "ALERT: ".$str."\n";
+    $this->alarms[] = $str;
 
   }
 
 
   function warning($str){
     echo "WARN: ".$str."\n";
-
+    $this->warnings[] = $str;
   }
 
 
 
   function info($str){
     echo $str."\n";
-
+    $this->infos[] = $str;
   }
 
 
 
+  function __destruct() {
+    echo "Notify: Processing queued notifications\n\n";
+  }
 
 
 

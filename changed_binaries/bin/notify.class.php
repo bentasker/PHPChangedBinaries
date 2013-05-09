d6ef9189b1ff8240c7f5cc16e03a1c90667d818f
Ben Tasker <github@bentasker.co.uk>
2013-05-09 18:07:10 +0100

    Added mail notification option

--- changed_binaries/bin/notify.class.php
+++ changed_binaries/bin/notify.class.php
@@ -6,7 +6,13 @@ class changedbinariesNotify{
    protected $alarms = array();
    protected $warnings = array();
    protected $infos = array();
-    
+   protected $config;
+
+
+  function __construct(){
+    require dirname(__FILE__)."/../config/notifications.php";
+    $this->config = $notify;
+  }
 
   function alarm($str){
     echo "ALERT: ".$str."\n";
@@ -31,6 +37,29 @@ class changedbinariesNotify{
 
   function __destruct() {
     echo "Notify: Processing queued notifications\n\n";
+    $msg = date('Y-m-d H:i:s') . ": Changed Binaries output follows\n".
+	  count($this->alarms) . " Alarms\n" . count($this->warnings) . " Warnings\n" . count($this->infos) . " Information messages\n\n";
+
+    if (!$this->config['enabled'] || empty($this->config['address'])){
+      return;
+    }
+
+    if ($this->config['alarm']){
+	$msg .=  "\n\nALARMS\n\n" . implode("\n",$this->alarms);
+    }
+
+    if ($this->config['warning']){
+	$msg .=  "\n\nWARNINGS\n\n" . implode("\n",$this->warnings);
+    }
+
+    if ($this->config['info']){
+	$msg .=  "\n\nInformational Messages\n\n" . implode("\n",$this->infos);
+    }
+
+    $msg .= "\n Report ends";
+
+    mail($this->config['address'],str_replace("[SERVER]",getenv('HOSTNAME'),$this->config['subject']),$msg);
+
   }
 
 
---
+++ changed_binaries/config/notifications.php
@@ -0,0 +1,16 @@
+<?php
+
+$notify['enabled'] = true;
+$notify['address'] = 'changedbins@bentasker.co.uk';
+
+// The email subject to use. [SERVER] will be replaced with the machines hostname
+$notify['subject'] = 'Changed Binaries report for [SERVER]';
+
+// Which output levels should be sent?
+$notify['info'] = false;
+$notify['warning'] = true;
+$notify['alarm'] = true;
+
+
+
+?> 

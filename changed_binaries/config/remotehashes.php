780fc877e363366b73ffe46a85e4c02c3023e4a6
Ben Tasker <github@bentasker.co.uk>
2013-05-09 18:07:35 +0100

    Started designing remote hashes option

---
+++ changed_binaries/config/remotehashes.php
@@ -0,0 +1,19 @@
+<?php
+/** Configuration file for RemoteHashStore integration
+*
+*/
+
+
+
+$remote_store_enabled = 1;
+$api_server = 'http://api.bentasker.co.uk/hashstore';
+$api_key = '1234';
+
+// Don't change this!
+$api_secret = '6789';
+
+
+
+
+
+ 

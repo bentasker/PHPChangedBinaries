98f35f82cfd9c5f842edb0143bcd655a188a37dd
Ben Tasker <github@bentasker.co.uk>
2013-05-09 12:56:21 +0100

    Codestyle fixes and implemented command line option

--- changed_binaries/bin/changed_binaries.php
+++ changed_binaries/bin/changed_binaries.php
@@ -4,98 +4,136 @@ error_reporting(0);
 
 class changedbinariesmain{
 
-function getpath(){
-  $this->path = explode(":",getenv("PATH"));
-  return $this->path;
-}
+    protected $path;
+    protected $action;
 
 
-function setadditional($dir){
-  if (!in_array($dir,$this->path)){
-    $this->path[] = $dir;
-  }
-}
-
-
-function checkfiles($dir){
-
-  if (!is_dir($dir) || !$h = opendir($dir)){
-    return false;
-  }
-
-  while (false !== ($entry = readdir($h))){
-    if ($entry == '.' || $entry == '..'){
-      continue;
-    }
-    $trg = $dir."/".$entry;
-
-    // If it's a dir, we want to recurse
-    if (is_dir($trg) && !is_link($trg)){
-      $this->checkfiles($trg);
-    }elseif (is_link($trg) && is_dir($trg)){
-      continue; // we'll work this bit out later
+    function getpath(){
+      $this->path = explode(":",getenv("PATH"));
+      return $this->path;
     }
 
 
-    $this->storeHash($trg);
-
-  }
-
-}
-
-
-function storeHash($file){
-  $fname = sha1($file);
-  $hash = $this->calcHash($file);
-  echo "Storing hash for $file in ". dirname(__FILE__)."/../db/$fname.def\n";
-
-  $f = fopen(dirname(__FILE__)."/../db/$fname.def",'w');
-
-  $str = "<?php \$hsh='$hash'; \$store='".time()."';?>";
-  fwrite($f,$str);
-  fclose($f);
-
-}
+    function setadditional($dir){
+      if (!in_array($dir,$this->path)){
+	$this->path[] = $dir;
+      }
+    }
 
 
-function calcHash($file){
-  return hash_file('sha512',$file);
-}
+    function setaction($act){
+      $this->action = $act;
+    }
 
 
+    function checkfiles($dir){
+      $fn = $this->action."Hash";
 
-function checkHash($file){
+      if (!is_dir($dir) || !$h = opendir($dir)){
+	return false;
+      }
 
-  $cur = $this->calcHash($file);
-  $stored = $this->loadDef($file);
+      while (false !== ($entry = readdir($h))){
+	if ($entry == '.' || $entry == '..'){
+	  continue;
+	}
+	$trg = $dir."/".$entry;
 
-  if ($cur != $stored){
-    echo "ALERT: $file has changed\n";
-  }
+	// If it's a dir, we want to recurse
+	if (is_dir($trg) && !is_link($trg)){
+	  $this->checkfiles($trg);
+	}elseif (is_link($trg) && is_dir($trg)){
+	  continue; // we'll work this bit out later
+	}
 
-}
 
+	$this->$fn($trg);
 
-function loadDef($file){
+      }
 
-  $fname = sha1($file);
-
-  if (!include(dirname(__FILE__)."/../db/$fname.def")){
-    echo "WARN: Could not load definition for $file\n";
-    return false;
-  }
-
-  return $hsh;
-}
+    }
 
 
 
-function init(){
-  foreach ($this->path as $path){
-  $this->checkfiles($path);
-  }
+    function storeHash($file){
+      $fname = sha1($file);
+      $hash = $this->calcHash($file);
+      echo "Storing hash for $file in ". dirname(__FILE__)."/../db/$fname.def\n";
 
-}
+      $f = fopen(dirname(__FILE__)."/../db/$fname.def",'w');
+
+      $str = "<?php \$hsh='$hash'; \$store='".time()."';?>";
+      fwrite($f,$str);
+      fclose($f);
+
+    }
+
+
+    function calcHash($file){
+      return hash_file('sha512',$file);
+    }
+
+
+
+    function checkHash($file){
+
+      $cur = $this->calcHash($file);
+      $stored = $this->loadDef($file);
+
+      if ($cur != $stored){
+	echo "ALERT: $file has changed\n";
+      }else{
+	echo "FINE: $file unchanged since last update\n";
+      }
+
+    }
+
+
+    /** TODO: Do something slightly more useful!
+    *
+    */
+    function fileChanged($file){
+      echo "ALERT: $file has changed\n";
+
+    }
+
+
+    /** TODO: Do something slightly more useful!
+    *
+    */
+    function fileUnChanged($file){
+      echo "FINE: $file unchanged since last update";
+    }
+
+
+
+    function loadDef($file){
+
+      $fname = sha1($file);
+
+      if (!include(dirname(__FILE__)."/../db/$fname.def")){
+	echo "WARN: Could not load definition for $file\n";
+	return false;
+      }
+
+      return $hsh;
+    }
+
+
+
+    function init(){
+      foreach ($this->path as $path){
+      $this->checkfiles($path);
+      }
+
+    }
+
+
+
+    function getInput($msg){
+      fwrite(STDOUT,"$msg: ");
+      return trim(fgets(STDIN));
+    }
 
 
 
@@ -115,6 +153,26 @@ if (is_dir('/usr/local/cpanel/bin/')){
 if (is_dir('/usr/local/psa/bin/')){
   $cbins->setadditional('/usr/local/psa/bin/');
 }
+
+
+/** TODO: Implement proper password control
+*
+*/
+if ($argv[1] == "-upd"){
+
+  if ($cbins->getInput("Password") == 1234){
+    $cbins->setaction('store');
+  }else{
+    echo "Incorrect password";
+    die;
+  }
+
+}else{
+  $cbins->setaction('check');
+}
+
+
+
 
 $cbins->init();
 
